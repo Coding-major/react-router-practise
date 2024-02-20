0ec2bb80337ee97a43b3a890fa82141182f1b27b
coding-major <shehumustapham@gmail.com>
2024-02-20 20:32:40 +0100

    added Link to tag

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ function App() {
   return (
     <BrowserRouter>
       <main>
+        <header>
+          <nav>
+            <h1>The jobs guy</h1>
+            <Link to='/'>Home</Link>
+            <Link to="about">About</Link>
+          </nav>
+        </header>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="about" element={<About />} />
